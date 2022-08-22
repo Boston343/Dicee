fcc677d9cb36dbc8dd0d222296abdd63da690412
Boston343 <branden.austin1@gmail.com>
2022-08-21 22:45:57 -0400

    simplified the js by removing the switch statements and just using string appending to pick the dice image

--- js/index.js
+++ js/index.js
@@ -1,69 +1,18 @@
 
 var randomNumber1;
+var randomNumber2;
+var randomDiceImg;
 
 // generate random numbers from 1 to 6
 randomNumber1 = Math.floor ( 6 * Math.random() ) + 1;
 randomNumber2 = Math.floor ( 6 * Math.random() ) + 1;
 
-// change dice image 1 based upon the random number
-switch (randomNumber1) {
-    case 1:
-        document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-        break;
+// change dice images based upon the random number
+randomDiceImg = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomDiceImg);
 
-    case 2:
-        document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-        break;
-
-    case 3:
-        document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-        break;
-
-    case 4:
-        document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-        break;
-
-    case 5:
-        document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-        break;
-
-    case 6:
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        break;
-
-    default:
-        break;
-}
-
-// change dice image 2 based upon the random number
-switch (randomNumber2) {
-    case 1:
-        document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-        break;
-
-    case 2:
-        document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-        break;
-
-    case 3:
-        document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-        break;
-
-    case 4:
-        document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-        break;
-
-    case 5:
-        document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-        break;
-
-    case 6:
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        break;
-
-    default:
-        break;
-}
+randomDiceImg = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomDiceImg);
 
 // change title to reflect who won the dice toss
 if (randomNumber1 > randomNumber2) {
